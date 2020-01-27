ac064cd388e56002868e37d8b4df5449b54bd0e8
r-ym <ramanyachi@gmail.com>
2020-01-27 16:30:59 +0900

    q1 done

---
+++ p1/target/debug/deps/p1-2a928d911fd7e45a.d
@@ -0,0 +1,5 @@
+/Users/Raman/Documents/GitHub/Advent-of-code-19/p1/target/debug/deps/p1-2a928d911fd7e45a: src/main.rs
+
+/Users/Raman/Documents/GitHub/Advent-of-code-19/p1/target/debug/deps/p1-2a928d911fd7e45a.d: src/main.rs
+
+src/main.rs:
---
+++ p1/target/debug/p1.d
@@ -0,0 +1,1 @@
+/Users/Raman/Documents/GitHub/Advent-of-code-19/p1/target/debug/p1: /Users/Raman/Documents/GitHub/Advent-of-code-19/p1/src/main.rs
